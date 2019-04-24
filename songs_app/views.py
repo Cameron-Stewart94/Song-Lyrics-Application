d2c6ec26d1120e8982ac441ee4b216b425f51918
Cameron Stewart <cameron.stewart246@gmail.com>
2019-04-24 11:42:06 +0100

    Handles input errors

--- songs_app/views.py
+++ songs_app/views.py
@@ -19,18 +19,22 @@ def index(request):
             artist_value = artist_form.cleaned_data['artist']
             song_value = song_form.cleaned_data['song']
             web_lyrics = Lyrics(artist_value, song_value)
-            web_lyrics.fetch_data()
-            song = song_form.save(commit=False)
-            artist, created = Artist.objects.get_or_create(artist = web_lyrics.artist_name)
-            song.lyrics = web_lyrics.song_lyrics
-            song.artist = artist
-            song.song = web_lyrics.song_name
-            case_artist = web_lyrics.artist_name
-
             try:
-                song.save()
+                web_lyrics.fetch_data()
             except:
-                return render(request, 'songs_app/lyrics.html', {'lyrics': song.lyrics, 'artist': case_artist, 'song': song.song})
+                return render(request, 'songs_app/invalid_choice.html')
+            else:
+                song = song_form.save(commit=False)
+                artist, created = Artist.objects.get_or_create(artist = web_lyrics.artist_name)
+                song.lyrics = web_lyrics.song_lyrics
+                song.artist = artist
+                song.song = web_lyrics.song_name
+                case_artist = web_lyrics.artist_name
+
+                try:
+                    song.save()
+                except:
+                    return render(request, 'songs_app/lyrics.html', {'lyrics': song.lyrics, 'artist': case_artist, 'song': song.song})
 
         return render(request, 'songs_app/lyrics.html', {'lyrics': song.lyrics, 'artist': case_artist, 'song': song.song})
 
