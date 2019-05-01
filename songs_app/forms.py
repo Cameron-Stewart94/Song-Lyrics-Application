e6aa7b5017e6ee1547698132a2574faa5e0fd057
Cameron Stewart <cameron.stewart246@gmail.com>
2019-05-01 10:41:11 +0100

    Adds form comments

--- songs_app/forms.py
+++ songs_app/forms.py
@@ -2,23 +2,29 @@ from django import forms
 from songs_app.models import Artist, Song
 
 class ArtistChoice(forms.ModelForm):
+# Class creates form for artist choice and links to Artist model
 
     class Meta():
         model = Artist
         fields = '__all__'
+        # Links to all Artist fields (Only one in this case)
 
         labels = {'artist': ''}
         widgets = {
         'artist': forms.TextInput(attrs={'placeholder': 'Enter artist here'}),
     }
-
+        # Removes label and adds placeholder to form
 
 
 class SongChoice(forms.ModelForm):
+    # Class creates form for song choice and links to Song model
+
     class Meta():
         model = Song
         fields = ('song',)
+        # Links only to song field in Song model as artist fiels is linked to Artist model through a foreign key
         labels = {'song': ''}
         widgets = {
         'song': forms.TextInput(attrs={'placeholder': 'Enter song here'}),
     }
+        # Removes label and adds placeholder to form
